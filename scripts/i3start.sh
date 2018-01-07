01645f3a2748cae528a53f0ca33e5be590867255
Andrew T <act.webdedigns@gmail.com>
2018-01-07 11:30:50 -0600

    Added scripts

---
+++ scripts/i3start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+compton -b
+$HOME/.config/polybar/launch.sh
+$HOME/scripts/wallpapers.sh
+setxkbmap -option caps:escape
+
---
+++ scripts/wallpapers.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+feh --bg-scale ~/Pictures/wallpapers/The_Great_Wave_off_Kanagawa.jpg
